794e5f93404732b11f4487ed140f688ba429be5b
Reality <ighoafokereality1@gmail.com>
2025-03-24 12:15:09 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  fetch("http://localhost:1998", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      message: "can you hear me server if yes pls respond",
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else if (response.status == 204) {
+        return;
+      } else {
+        console.log("server didnt respond");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
